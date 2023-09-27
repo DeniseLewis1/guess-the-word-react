a253251f13efacd444df07648e4eac76bf7a70a6
Denise <deniselewis1120@gmail.com>
2023-09-27 14:14:42 -0700

    Replace setState with state updater function

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ const App = () => {
     const words = await response.text();
     const wordArray = words.split("\n");
     const randomIndex = Math.floor(Math.random() * wordArray.length);
-    this.setState({ word: wordArray[randomIndex].trim() });
-    this.placeholder(this.state.word);
+    setWord(wordArray[randomIndex].trim());
+    placeholder(word);
   }
 /*
   // Get a random word
@@ -39,26 +39,27 @@ const App = () => {
   const placeholder = word => {
     let placeholderLetters = [];
     for (let letter of word) {
-        placeholderLetters.push("●");
+      placeholderLetters.push("●");
     }
     
-    this.setState({ wordInProgress: placeholderLetters.join("") });
+    setWordInProgress(placeholderLetters.join(""));
   };
 
   // Guess button clicked
   const guessButton = e => {
     e.preventDefault();
-    const guess = this.state.letterInput;
-    this.setState({ letterInput: "", message: "" });
-    const guessChecked = this.inputCheck(guess);
+    const guess = letterInput;
+    setLetterInput("");
+    setMessage("");
+    const guessChecked = inputCheck(guess);
     
     if (guessChecked) {
-        this.makeGuess(guessChecked);
+      makeGuess(guessChecked);
     }
   };
 
   const updateInput = e => {
-    this.setState({ letterInput: e.target.value });
+    setLetterInput(e.target.value);
   };
 
   // Validate player input
@@ -66,73 +67,79 @@ const App = () => {
     const acceptedLetter = /[a-zA-Z]/;
 
     if (input === "") {
-        this.setState({ message: `Please enter a letter.` });
+      setMessage(`Please enter a letter.`);
     } else if (input.length > 1) {
-        this.setState({ message: `Please enter a single letter.` });
+      setMessage(`Please enter a single letter.`);
     } else if (!input.match(acceptedLetter)) {
-        this.setState({ message: `Please enter a letter from A to Z.` });
+      setMessage(`Please enter a letter from A to Z.`);
     } else {
-        return input;
+      return input;
     }
   };
 
   // Check if letter has been guessed already
   const makeGuess = guess => {
     guess = guess.toUpperCase();
-    if (this.state.guessedLetters.includes(guess)) {
-        this.setState({ message: `You've already guessed that letter, try again!` });
+    if (guessedLetters.includes(guess)) {
+        setMessage(`You've already guessed that letter, try again!`);
     } else {
-        this.state.guessedLetters.push(guess);
-        this.showGuesses();
-        this.countRemainingGuesses(guess);
-        this.updateWordInProgress(this.state.guessedLetters);
+        guessedLetters.push(guess);
+        showGuesses();
+        countRemainingGuesses(guess);
+        updateWordInProgress(guessedLetters);
     }
   };
 
   // Display guessed letters
   const showGuesses = () => {
-    this.setState({ guessedLettersElement: [] });
-    const guessedLettersList = this.state.guessedLetters.map((letter) => <li key={letter}>{letter}</li>);
-    this.setState({ guessedLettersElement: guessedLettersList });
+    setGuessedLettersElement([]);
+    const guessedLettersList = guessedLetters.map((letter) => <li key={letter}>{letter}</li>);
+    setGuessedLettersElement(guessedLettersList);
   };
 
   // Update word in progress with correct letters
   const updateWordInProgress = guessedLetters => {
-    const wordUpper = this.state.word.toUpperCase();
+    const wordUpper = word.toUpperCase();
     const wordArray = wordUpper.split("");
     let showWord = [];
 
     for (let letter of wordArray) {
         if (guessedLetters.includes(letter)) {
-            showWord.push(letter);
+          showWord.push(letter);
         } else {
-            showWord.push("●");
+          showWord.push("●");
         }
     }
 
-    this.setState({ wordInProgress: showWord.join("") });
-    this.checkWin(showWord.join(""));
+    setWordInProgress(showWord.join(""));
+    //checkWin(showWord.join(""));
+    checkWin();
   };
 
   // Determine number of remaining guesses
   const countRemainingGuesses = guess => {
-    const wordUpper = this.state.word.toUpperCase();
+    const wordUpper = word.toUpperCase();
     if (wordUpper.includes(guess)) {
-        this.setState({ message: `Good guess! The word has the letter ${guess}.` });
+      setMessage(`Good guess! The word has the letter ${guess}.`);
     } else {
-        this.setState({ message:  `Sorry, the word has no ${guess}.`, remainingGuesses: this.state.remainingGuesses -= 1 });
+      setMessage(`Sorry, the word has no ${guess}.`);
+      setRemainingGuesses(remainingGuesses -= 1);
     }
 
-    if (this.state.remainingGuesses === 0) {
-        this.setState({ message: `Game over! The word is <span className="highlight">${wordUpper}</span>.`, newGame: true });
+    if (remainingGuesses === 0) {
+      setMessage(`Game over! The word is <span className="highlight">${wordUpper}</span>.`);
+      setNewGame(true);
 //        this.startOver();
     }
   };
 
   // Check if player won
-  const checkWin = (word) => {
-    if (word === this.state.word.toUpperCase()) {
-      this.setState({ wonGame: true, message: `You guessed correct the word! Congrats!`, newGame: true });
+  //const checkWin = (word) => {
+  const checkWin = () => {
+    if (wordInProgress === word.toUpperCase()) {
+      setWonGame(true);
+      setMessage(`You guessed correct the word! Congrats!`);
+      setNewGame(true);
 //        message.classList.add("win");
 //        this.startOver();
     }
@@ -157,39 +164,42 @@ const App = () => {
 //    this.getWord();
 */
     //this.componentDidMount();
-    this.getWord();
-    this.setState({ wonGame: false, message: "", guessedLettersElement: "", remainingGuesses: 8, guessedLetters: [], newGame: false });
+    getWord();
+    setWonGame(false);
+    setMessage("");
+    setGuessedLettersElement("");
+    setRemainingGuesses(8);
+    setGuessedLetters([]);
+    setNewGame(false);
   };
 
 
 
 
-  //render() {
-    return (
-      <div className="App">
-        <div className="container">
-          <h1>
-            <img className="logo" src={logo} alt="Guess The Word" />
-          </h1>
-          {this.state.wonGame ? 
-            <p className="message win"><p className="highlight">{this.state.message}</p></p> : 
-            <p className="message">{this.state.message}</p>}
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>
+          <img className="logo" src={logo} alt="Guess The Word" />
+        </h1>
+        {wonGame ? 
+          <p className="message win"><p className="highlight">{message}</p></p> : 
+          <p className="message">{message}</p>}
 {/*          <p className={this.state.wonGame ? "message win" : "message"}>{this.state.message}</p> */}
-          <p className="word-in-progress">{this.state.wordInProgress}</p>
-          <p className={this.state.newGame ? "remaining hide" : "remaining"}>You have {this.state.remainingGuesses} {this.state.remainingGuesses === 1 ? "guess" : "guesses"} remaining.</p>
-          <ul className={this.state.newGame ? "guessed-letters hide" : "guessed-letters"}>{this.state.guessedLettersElement}</ul>
-          <form action="" className="guess-form">
-            <label for="letter">Type one letter:</label>
-            <input type="text" name="letter" className="letter" value={this.state.letterInput} onChange={this.updateInput} />
-            <div className="form-element button-element">
-              <button className={this.state.newGame ? "guess hide" : "guess"} onClick={this.guessButton}>Guess!</button>
-            </div>
-          </form>
-          <button className={this.state.newGame ? "play-again" : "play-again hide"} onClick={this.playAgainButton}>Play Again!</button>
-        </div>
+        <p className="word-in-progress">{wordInProgress}</p>
+        <p className={newGame ? "remaining hide" : "remaining"}>You have {remainingGuesses} {remainingGuesses === 1 ? "guess" : "guesses"} remaining.</p>
+        <ul className={newGame ? "guessed-letters hide" : "guessed-letters"}>{guessedLettersElement}</ul>
+        <form action="" className="guess-form">
+          <label for="letter">Type one letter:</label>
+          <input type="text" name="letter" className="letter" value={letterInput} onChange={updateInput} />
+          <div className="form-element button-element">
+            <button className={newGame ? "guess hide" : "guess"} onClick={guessButton}>Guess!</button>
+          </div>
+        </form>
+        <button className={newGame ? "play-again" : "play-again hide"} onClick={playAgainButton}>Play Again!</button>
       </div>
-    );
-  //}
+    </div>
+  );
 }
 
 export default App;
